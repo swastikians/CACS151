ec97873aa79098994cfaa6824df63222aa29d9f5
Sandesh Shrestha <126145622+sthaSandesh@users.noreply.github.com>
2023-03-27 21:09:01 +0545

    Update 04_size.c

--- basics/04_size.c
+++ basics/04_size.c
@@ -6,10 +6,10 @@ int main(int argc, char const *argv[])
     float c;
     double d;
     long l;
-    printf("size of char %lu\n",sizeof(a));
-    printf("size of int %lu\n",sizeof(b));
-    printf("size of float %lu\n",sizeof(c));
-    printf("size of double %lu\n",sizeof(d));
-    printf("size of long %lu\n",sizeof(l));
+    printf("size of char %lu\n",sizeof(a)); //size of char 1  
+    printf("size of int %lu\n",sizeof(b));//size of int 4   
+    printf("size of float %lu\n",sizeof(c));//ize of float 4
+    printf("size of double %lu\n",sizeof(d));//size of double 8
+    printf("size of long %lu\n",sizeof(l));//size of long 4 
     return 0;
 }
