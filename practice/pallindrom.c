122ff7c33c56cd5562e7cf6c8e71ababaf84205c
Sandesh Shrestha <126145622+sthaSandesh@users.noreply.github.com>
2023-03-27 20:50:20 +0545

    Add files via upload

---
+++ practice/pallindrom.c
@@ -0,0 +1,27 @@
+// Program to check weather the number is pallindrome or not ;
+
+#include <stdio.h>
+int main(int argc, char const *argv[])
+{
+    int num, sum = 0, rem, temp;
+    printf("Enter any number");
+    scanf("%i", &num);
+    temp = num;
+
+    while (num > 0)
+    {
+        rem = num % 10;
+        sum = (sum * 10) + rem;
+        num = num / 10;
+    }
+    if (sum == temp)
+    {
+        printf("The number is Pallindrome");
+    }
+    else
+    {
+        printf("The number is not pallindrome");
+    }
+
+    return 0;
+}
