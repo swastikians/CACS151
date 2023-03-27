cdb206f76fed25338329c53815650b1844a9a9f6
Archaeopteryx <46562212+skdhg@users.noreply.github.com>
2023-03-27 20:59:42 +0545

    Update and rename pallindrom.c to 11_palindrome.c
    plus formatting

--- practice/11_palindrome.c
+++ practice/11_palindrome.c
@@ -1,25 +1,21 @@
-// Program to check weather the number is pallindrome or not ;
+// Program to check weather the number is palindrome or not ;
 
 #include <stdio.h>
-int main(int argc, char const *argv[])
-{
+int main() {
     int num, sum = 0, rem, temp;
     printf("Enter any number");
     scanf("%i", &num);
     temp = num;
 
-    while (num > 0)
-    {
+    while (num > 0) {
         rem = num % 10;
         sum = (sum * 10) + rem;
         num = num / 10;
     }
-    if (sum == temp)
-    {
+    
+    if (sum == temp) {
         printf("The number is Pallindrome");
-    }
-    else
-    {
+    } else {
         printf("The number is not pallindrome");
     }
 
