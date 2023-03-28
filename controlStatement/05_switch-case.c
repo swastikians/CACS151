0bdd1183e767980f4df1be2e36bd1714b7f18b4f
theanjali01 <anjalish6061@gmail.com>
2023-03-28 17:52:54 +0545

    added a c program in controlStatement

--- controlStatement/05_switch-case.c
+++ controlStatement/05_switch-case.c
@@ -1,21 +1,22 @@
-#include <stdio.h>
+// #include <stdio.h>
 
-int main() {
-    int number;
-    printf("Enter a number: ");
-    scanf("%d", &number);
+// int main() {
+//     int number;
+//     printf("Enter a number 10 or 20 or 30: ");
+//     scanf("%d", &number);
 
-    switch (number) {
-        case 10:
-            printf("The number is equal to 10");
-            break;
-        case 20:
-            printf("The number is equal to 20");
-            break;
-        case 30:
-            printf("The number is equal to 30");
-            break;
-    }
+//     switch (number) {
+//         case 10:
+//             printf("The number is equal to 10");
+//             break;
+//         case 20:
+//             printf("The number is equal to 20");
+//             break;
+//         case 30:
+//             printf("The number is equal to 30");
+//             break;
+//     }
 
-    return 0;
-}
+//     return 0;
+// }
+
