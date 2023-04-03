8e8662e86a88816086c28a0613db0c21510bd89e
skdhg <46562212+skdhg@users.noreply.github.com>
2023-04-03 13:53:38 +0545

    rewrite largest num

--- labWork/LAB2/01.c
+++ labWork/LAB2/01.c
@@ -3,15 +3,23 @@
 #include <stdio.h>
 
 int main() {
-    int a, b, c;
+    int a, b, c, max;
 
     printf("\nEnter 3 numbers: ");
     scanf("%d %d %d", &a, &b, &c);
 
-    if ((a >= b && a >= c) || (b >= a && b >= c) || (c >= a && c >= b)) {
-        int max = (a >= b && a >= c) ? a : (b >= a && b >= c) ? b : c;
-        printf("%d is the largest number among %d, %d and %d.\n", max, a, b, c);
+    if (a >= b && a >= c) {
+        max = a;
+    } else if (b >= a && b >= c) {
+        max = b;
+    } else if (c >= a && c >= b) {
+        max = c;
+    } else {
+        printf("\nCould not determine the largest number.\n");
+        return 1;
     }
+
+    printf("%d is the largest number among %d, %d and %d.\n", max, a, b, c);
 
     return 0;
 }
